a4d8ad1e45c39df787d15c88998d6ab5c6c7c779
fidankhalilova <fidankhalilova@Fidans-MacBook-Pro.local>
2025-11-02 15:35:26 +0400

    fix: query

--- snikei-client/src/Provider/UseQueryProvider.tsx
+++ snikei-client/src/Provider/UseQueryProvider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,7 +14,13 @@ const queryClient = new QueryClient({
 
 const UseQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+
+        {children}
+      </QueryClientProvider>
+    </>
   );
 };
 
